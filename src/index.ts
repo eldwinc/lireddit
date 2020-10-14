9adf3a76e1735de595fbc6e6d78e8467b807518b
Eldwin <Eldwin@Eldwins-MacBook-Pro.local>
2020-10-14 07:39:19 -0700

    47.30 finish graphql server setup

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,37 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main= async ()=>{
     const orm= await MikroORM.init(microConfig); //connect to db
     await orm.getMigrator().up(); //autorun migrations, itll do this b4 anything after
-    const post= orm.em.create(Post,{title:'my 1st post'});
-    await orm.em.persistAndFlush(post);
+    
+    //graphql server setup w typegraphql schema
+    const app= express(); //create instance of express
+    const apolloServer= new ApolloServer({ 
+        schema: await buildSchema({
+            resolvers:[HelloResolver],
+            validate:false
+        })
+    });
+    apolloServer.applyMiddleware({app}); //creates a graphql endpt with express!
+    
+    app.listen(4000,()=>{console.log('server started on localhost:4000')}); //start server on port4k
+    //Test Express
+    // app.get('/',(_,res)=>{  //create path endpt via get, ignore req parameter by inputting an underscore. then sending msg to connected server
+    //     res.send('hello')
+    // });
+
+    // Create a Post, then execute it
+    // const post= orm.em.create(Post,{title:'my 1st post'}); //create action (post)
+    // await orm.em.persistAndFlush(post); //execute action (post)
     // await orm.em.nativeInsert(Post,{title:'my 2nd post'}); //causes an err during migration bc Date() isnt created bc nativeInsert doesnt allow access to the classes
+    
+    //List all posts
     // const posts= await orm.em.find(Post,{}); //lists all posts. need await or less sys wont wait for promise to come back n will consolelog nothing
     // console.log(posts);
 };
