dfda84eacd623a2a6b8558b6466208857e8a68df
Eldwin <Eldwin@Eldwins-MacBook-Pro.local>
2020-10-13 20:10:33 -0700

    32:30 npx mikro-orm migration:create

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello wrld");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main= async ()=>{
+    const orm= await MikroORM.init(microConfig);
+    const post= orm.em.create(Post,{title:'my 1st post'});
+    await orm.em.persistAndFlush(post);
+    console.log("-----------------sql2");
+    await orm.em.nativeInsert(Post,{title:'my 2nd post'});
+};
+
+main().catch(err=>{
+    console.error(err);
+});
